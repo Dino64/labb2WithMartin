f1f9b4ea51551c95c3bee7e7bf18b61f7567b26a
Jussi <martin.jussi83@hotmail.se>
2021-04-26 13:57:38 +0200

    work it baby

---
+++ app/src/main/java/com/example/atm/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.atm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editTextEmail, editTextPassword;
+    private Button button;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextEmail = findViewById(R.id.loginEmail);
+        editTextPassword = findViewById(R.id.loginPassword);
+        button = findViewById(R.id.loginButton);
+        button.setOnClickListener(this);
+        progressBar = findViewById(R.id.progressBarLogin);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.loginButton:
+                login();
+                break;
+        }
+    }
+
+    private void login() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if (email.isEmpty()) {
+            editTextEmail.setError("please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Please provide Password");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 8) {
+            editTextPassword.setError("min password length should be 8 characters");
+            editTextPassword.requestFocus();
+            return;
+        } else {
+            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, System.class));
+
+        }
+
+
+    }
+}
+
